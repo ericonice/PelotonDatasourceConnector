f6935b8042ce020c739c01c7371392b2abf43f71
ericonice <ericonice@gmail.com>
2020-04-27 21:23:05 -0700

    Update properties of conntector

--- utils.js
+++ utils.js
@@ -1,39 +1,3 @@
-function ChunkyCache(cache, chunkSize){
-  return {
-    put: function (key, value, timeout) {
-      var json = JSON.stringify(value);
-      var cSize = Math.floor(chunkSize / 2);
-      var chunks = [];
-      var index = 0;
-      while (index < json.length){
-        cKey = key + "_" + index;
-        chunks.push(cKey);
-        cache.put(cKey, json.substr(index, cSize), timeout+5);
-        index += cSize;
-      }
-      
-      var superBlk = {
-        chunkSize: chunkSize,
-        chunks: chunks,
-        length: json.length
-      };
-      cache.put(key, JSON.stringify(superBlk), timeout);
-    },
-    get: function (key) {
-      var superBlkCache = cache.get(key);
-      if (superBlkCache != null) {
-        var superBlk = JSON.parse(superBlkCache);
-        chunks = superBlk.chunks.map(function (cKey){
-          return cache.get(cKey);
-        });
-        if (chunks.every(function (c) { return c != null; })){
-          return JSON.parse(chunks.join(''));
-        }
-      }
-    }
-  };
-};
-
 /**
  * Takes a timestamp, and parses it into the YYYY-MM-DD format.
  *
